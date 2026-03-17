ddd2ff89c0346ab409b91cf644ccd36374d3aa97
Thiago Kenji Uemura <tkenji-u@c1r3p4.42sp.org.br>
2026-03-16 21:45:14 -0300

    Ex02 and Ex03 made

---
+++ ex03/src/HumanB.cpp
@@ -0,0 +1,19 @@
+#include "Weapon.hpp"
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name) : _name(name), _weapon(NULL){}
+
+void HumanB::setWeapon(Weapon &weapon)
+{
+	this->_weapon = &weapon;
+}
+
+void HumanB::attack(void)
+{
+	if (this->_weapon)
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+	else
+		std::cout << this->_name << " has no weapon to attack with!" << std::endl;
+}
+
+HumanB::~HumanB() {}
